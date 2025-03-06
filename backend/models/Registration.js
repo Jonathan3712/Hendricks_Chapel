da6660dcad9ecaa91946c784b7c254d53651596f
Jonathan3712 <jonathanraj5678@gmail.com>
2025-03-06 14:27:13 -0500

    Fixed models and routes structure

--- backend/models/Registration.js
+++ backend/models/Registration.js
@@ -1,13 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-// Define the Registration schema
 const RegistrationSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  graduationYear: { type: Number, required: true },
-}, { timestamps: true }); // Adds createdAt & updatedAt timestamps
+  graduationYear: { type: Number, required: true }
+});
 
-// Create the Registration model
-const Registration = mongoose.model("Registration", RegistrationSchema);
-
-module.exports = Registration;
+module.exports = mongoose.model('Registration', RegistrationSchema);
